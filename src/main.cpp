5c5bd5febe2676eb898408cad0ad56c3bf98033b
msd921 <126858364+msd921@users.noreply.github.com>
2024-11-01 20:00:16 +0300

    create project

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿// serverFileManager.cpp: определяет точку входа для приложения.
+//
+
+#include "serverFileManager.h"
+#include "server.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake. " << Ap << endl;
+	return 0;
+}
